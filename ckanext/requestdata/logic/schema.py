b550d83662fb17d8cbb89ecca786e303f1f64d1a
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-03-09 15:57:54 +0100

    Remove 'data_shared' from schema

--- ckanext/requestdata/logic/schema.py
+++ ckanext/requestdata/logic/schema.py
@@ -5,7 +5,6 @@ from ckanext.requestdata.logic import validators
 
 not_missing = toolkit.get_validator('not_missing')
 not_empty = toolkit.get_validator('not_empty')
-boolean_validator = toolkit.get_validator('boolean_validator')
 package_name_exists = toolkit.get_validator('package_name_exists')
 email_validator = validators.email_validator
 
@@ -16,6 +15,5 @@ def request_create_schema():
         'organization': [not_empty, unicode],
         'email_address': [not_empty, email_validator],
         'message_content': [not_empty, unicode],
-        'package_name': [not_empty, package_name_exists],
-        'data_shared': [not_missing, boolean_validator]
+        'package_name': [not_empty, package_name_exists]
     }
