01c4c9b0dcf1dfa7efd76f4b48d823b8c8183d54
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-03-10 11:15:45 +0100

    Add Request data btn, add pop up contact form

---
+++ ckanext/requestdata/fanstatic/request-data.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/* request-data
+ *
+ * This JavaScript module adds a Bootstrap popover with contact form
+ * that sends mail to the resource owner
+ *
+ */
+
+this.ckan.module('request-data', function ($) {
+  return {
+
+  };
+});
