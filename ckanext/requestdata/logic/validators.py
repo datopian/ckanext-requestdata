9e631ae575ac9c1177c5dc497e77ce9137bba78e
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-03-13 15:55:07 +0100

    Add additional validators

--- ckanext/requestdata/logic/validators.py
+++ ckanext/requestdata/logic/validators.py
@@ -11,3 +11,18 @@ def email_validator(key, data, errors, context):
     except Exception:
         message = _('Please provide a valid email address.')
         errors[key].append(message)
+
+
+def state_validator(key, data, errors, context):
+    possible_state = ['new', 'open', 'archive']
+
+    if data[key] not in possible_state:
+        message = _('The state parameter must be new, open or archive.')
+        errors[key].append(message)
+
+
+def boolean_validator(key, data, errors, context):
+    if not isinstance(data[key], bool):
+        message = _('The {0} parameter must be a Boolean value.'
+                    .format(key[0]))
+        errors[key].append(message)
