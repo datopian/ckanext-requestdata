ab1d00788d3bf6a66c6da6461efc27982245f223
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-06-01 13:52:14 +0200

    Use CKAN module for arrow icon for archived requests

--- ckanext/requestdata/fanstatic/expand-dataset.js
+++ ckanext/requestdata/fanstatic/expand-dataset.js
@@ -1,27 +1,37 @@
-(function() {
+/* expand-dataset
+ *
+ * This JavaScript module handles toggling arrow icon for expand/contract 
+ *
+ */
 
-  var archiveRequestsArrow = $('.archive-requests-arrow')
+this.ckan.module('expand-dataset', function($) {
+  return {
+    initialize: function() {
+      $.proxyAll(this, /_on/)
 
-  archiveRequestsArrow.on('click', function(event) {
-    var arrow = $(this)
-    var iconRight = 'icon-chevron-right' 
-    var iconDown = 'icon-chevron-down'
-    var prefix = ''
+      this.el.on('click', this._onArrowClick)
+    },
+    _onArrowClick: function(event) {
+      var arrow = this.el.find('i')
+      var iconRight = 'icon-chevron-right' 
+      var iconDown = 'icon-chevron-down'
+      var prefix = ''
 
-    if (archiveRequestsArrow.hasClass('glyphicon')) {
-      prefix = 'glyph'
+      if (arrow.hasClass('glyphicon')) {
+        prefix = 'glyph'
+      }
+
+      iconRight = prefix + iconRight
+      iconDown = prefix + iconDown
+
+      if (arrow.hasClass(iconRight)) {
+        arrow.removeClass(iconRight)
+        arrow.addClass(iconDown)
+      } else if (arrow.hasClass(iconDown)) {
+        arrow.removeClass(iconDown)
+        arrow.addClass(iconRight)
+      }
     }
+  }
+})
 
-    iconRight = prefix + iconRight
-    iconDown = prefix + iconDown
-
-    if (arrow.hasClass(iconRight)) {
-      arrow.removeClass(iconRight)
-      arrow.addClass(iconDown)
-    } else if (arrow.hasClass(iconDown)) {
-      arrow.removeClass(iconDown)
-      arrow.addClass(iconRight)
-    }
-  })
-
-})()
